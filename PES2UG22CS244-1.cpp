be2bbd5896d9499f227297376684838326329256
adithyakr8055 <150340758+adithyakr8055@users.noreply.github.com>
2025-03-20 20:31:31 +0530

    Create PES2UG22CS244-1.cpp

---
+++ PES2UG22CS244-1.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, welcome to CC\n";
+  cout << "I have successfully built and run this file in L Section \n";
+  return 0;
+}
